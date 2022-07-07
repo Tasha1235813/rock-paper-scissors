1afa471156d048a2cd2e7d3e4196b68d8b273484
Tasha <Tasha.Bertch@gmail.com>
2022-07-06 19:22:26 -0700

    Declare variables and set up array of choices

---
+++ r-p-s.js
@@ -0,0 +1,31 @@
+let computerChoice;
+let playerChoice;
+let computerscore = 0;
+let playerScore = 0;
+
+playRound(playerChoice, computerChoice);
+
+    if (playerScore === 5 || computerScore === 5) {
+      declareWinner();
+    }
+
+
+
+
+
+const gameArray = ["Rock", "Paper", "Scissors"];
+let shoot = Math.random();
+let choices = gameArray.length;
+let randChoice = Math.floor(shoot*choices);
+let computerPlay = gameArray[randChoice]
+
+function computerPlay() {
+
+}
+
+
+
+
+function declareWinner() {
+
+}
